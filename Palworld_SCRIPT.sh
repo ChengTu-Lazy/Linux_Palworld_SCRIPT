19b107a09c9b44dea10c784465b6f3333deec607
ChengTu-Lazy <1549737287@qq.com>
2024-02-04 18:35:05 +0800

    新增常用配置修改功能

--- Palworld_SCRIPT.sh
+++ Palworld_SCRIPT.sh
@@ -2,9 +2,10 @@
 #History:
 # 2024年1月31日 诚徒开始编辑
 # 2024年2月2日 完成主要功能，实现一键开服关服，崩服自动重启，自动更新游戏，备份游戏存档功能
+# 2024年2月4日 新增配置文件修改功能
 
 #脚本版本
-SCRIPT_VERSION="1.0.0"
+SCRIPT_VERSION="1.0.1"
 # 脚本当前名称
 SCRIPT_NAME=$(basename "$0")
 # 脚本当前所在目录
@@ -15,6 +16,8 @@ ACCELERATION_URL="https://ghp.quickso.cn/https://github.com/ChengTu-Lazy//Linux_
 OS=$(awk -F = '/^NAME/{print $2}' /etc/os-release | sed 's/"//g' | sed 's/ //g' | sed 's/Linux//g' | sed 's/linux//g')
 #默认安装路径
 PALWORLD_DEFAULT_PATH="$HOME/.Palworld"
+#默认配置文件所在位置
+DEFAULTPALWORLDSETTINGS_PATH="$PALWORLD_DEFAULT_PATH/DefaultPalWorldSettings.ini"
 #默认存档所在位置
 PALWORLD_SAVES_PATH="$HOME/.Palworld/Pal/Saved"
 
@@ -267,6 +270,192 @@ checkprocess() {
 	fi
 }
 
+update_config_option() {
+    # 从参数中获取配置文件路径、选项名称和新的选项值
+    local config_file="$DEFAULTPALWORLDSETTINGS_PATH"
+    local option_name="$1"
+    local new_value="$2"
+
+    # 使用awk命令获取选项的当前值
+    get_current_config_value "$option_name"
+	
+    # 使用sed命令替换选项的当前值为新的选项值
+    sed -i "s/$option_name=$current_config_value/$option_name=$new_value/" "$config_file"
+}
+
+
+get_current_config_value(){
+	local config_file="$DEFAULTPALWORLDSETTINGS_PATH"
+    local option_name="$1"
+	current_config_value=$(grep -Po "(?<=$option_name=)[^,]+" "$config_file")
+}
+
+
+update_config(){
+	local config_option=$1
+	local prompt_message=$2
+	
+	echo_current_config_value "$config_option"
+
+	echo -e "\e[92m$prompt_message\e[0m"
+	read -r read_val
+	
+	update_config_option "$config_option" "$read_val"
+	echo "设置完成！"
+}
+
+update_config_with_num() {
+	local config_option=$1
+	local prompt_message=$2
+	
+	echo_current_config_value "$config_option"
+
+	echo -e "\e[92m$prompt_message\e[0m"
+	read -r read_val
+	
+	# 判断变量是否为数字
+	if [[ $read_val =~ ^[0-9]+([.][0-9]+)?$ ]]; then
+		update_config_option "$config_option" "$read_val"
+		echo -e "\e[92m设置完成！\e[0m"
+	else
+		echo -e "\e[31m输入的并不是数字，请重新设置\e[0m"
+		change_config
+	fi
+}
+
+update_config_with_str(){
+	local config_option=$1
+	local prompt_message=$2
+	
+	echo_current_config_value "$config_option"
+
+	echo -e "\e[92m$prompt_message\e[0m"
+	read -r read_val
+	
+	update_config_option "$config_option" "\"$read_val\""
+	echo "设置完成！"
+}
+
+update_config_with_bool(){
+	local config_option=$1
+	local prompt_message=$2
+	
+	echo_current_config_value "$config_option"
+
+	echo -e "\e[92m$prompt_message\e[0m"
+	read -r is_open_pvp
+	if [[ $is_open_pvp = "Y" ]] || [[ $is_open_pvp = "y" ]]
+	then
+		update_config_option "bEnablePlayerToPlayerDamage" "True"
+		echo PVP已开启!
+	else
+		update_config_option "bEnablePlayerToPlayerDamage" "False"
+		echo PVP关闭!
+	fi
+}
+
+echo_current_config_value(){
+	local config_option=$1
+	get_current_config_value "$config_option"
+	echo -e "\e[92m当前值为：$current_config_value\e[0m"
+}
+
+# 更改配置
+change_config() {
+	while :; do
+		echo "===========================!!!请输入需要进行的操作序号!!!==========================="
+		echo "                                                                                  "
+		echo "	[1]设置服务器名          [2]设置服务器描述        [3]设置服务器密码			"
+		echo "                                                                                  "
+		echo "	[4]设置服务器人数        [5]设置管理员密码        [6]设置服务器端口"
+		echo "                                                                                  "
+		echo "	[7]设置RCON端口号        [8]修改PVP设置(默认关)   [9]设置营地最大数"
+		echo "                                                                                  "
+		echo "	[10]设置营地精灵最大数   [11]设置掉落物最大数量   [12]设置精灵生成倍率"
+		echo "                                                                                  "
+		echo "	[13]设置经验倍率         [14]设置捕捉倍率         [15]设置孵蛋时间"
+		echo "                                                                                  "
+		echo "	[16]设置死亡掉落         "
+		echo "                                                                                  "
+		echo "===========================!!!输入其他值直接返回主菜单!!!==========================="
+		echo "                                                                                  "
+		echo -e "\e[92m请输入命令代号:\e[0m"
+		read -r consoleinfo
+		(case $consoleinfo in
+			1) 
+				update_config_with_str ServerName 请输入服务器名
+				;;
+			2)
+				update_config_with_str ServerDescription 请输入服务器描述
+				;;
+			3)
+				update_config_with_str ServerPassword 请输入服务器密码
+				;;
+			4)
+				update_config_with_num ServerPlayerMaxNum 请输入服务器人数（上限为32）
+				;;
+			5)
+				update_config_with_str AdminPassword 请输入管理员密码
+				;;
+			6)
+				update_config_with_num PublicPort 请输入服务器端口，默认8211
+				;;
+			7)
+				update_config_with_num RCONPort 请输入RCON端口，默认25575
+				;;
+			8)
+				update_config_with_bool "bEnablePlayerToPlayerDamage" 输入Y/y开启PVP回车等其他为关闭PVP，默认关闭
+				;;
+			9)
+				update_config_with_num BaseCampMaxNum 请输入营地最大数0-5000，默认128
+				;;
+			10)
+				update_config_with_num BaseCampWorkerMaxNum 请输入营地中最多精灵数1-20，默认15
+				;;
+			11)
+				update_config_with_num DropItemMaxNum 请输入世界掉落物上限0-5000，默认3000
+				;;
+			12)
+				update_config_with_num PalSpawnNumRate 请输入精灵生成倍率0.5-3.0，默认1
+				;;
+			13)
+				update_config_with_num ExpRate 请输入玩家经验倍率0.1-20.0，默认1
+				;;	
+			14)
+				update_config_with_num PalCaptureRate 请输入捕捉倍率0.5-2.0，默认1
+				;;	
+			15)
+				update_config_with_num PalEggDefaultHatchingTime 请输入巨大蛋孵蛋时间0-240H，默认72H
+				;;	
+			16)
+				echo_current_config_value "DeathPenalty"
+				echo -e "对应情况为\n1.None=不掉落任何物品\n2.Item=掉落装备以外的道具\n3.ItemAndEquipment=掉落所有物品\n4.All=掉落所有物品及队内帕鲁"
+				echo -e "\e[92m请输入对应序号：\n1.不掉落任何物品\n2.掉落装备以外的道具\n3.掉落所有物品\n4.掉落所有物品及队内帕鲁\e[0m"
+				read -r read_val
+				if [ "$read_val" = "1" ] 
+				then
+					update_config_option "DeathPenalty" "None"
+				elif [ "$read_val" = "2" ] 
+				then
+					update_config_option "DeathPenalty" "Item"
+				elif [ "$read_val" = "3" ] 
+				then
+					update_config_option "DeathPenalty" "ItemAndEquipment"
+				elif [ "$read_val" = "4" ] 
+				then
+					update_config_option "DeathPenalty" "All"
+				else
+					echo "请输入正确序号"
+					change_config
+				fi
+				echo "设置完成！"
+				;;	
+			*)
+				main
+				;;
+			esac)
+	done
+}
 
 #主菜单
 main() {
@@ -289,9 +478,11 @@ main() {
 		printf "%s\n" "          服务器当前状态: $server_staut  自动更新进程状态: $autoUpdateServer_staut  "
 		echo "============================================================"
 		echo "                                          	                 "
-		echo "  [1]启动服务器         [2]关闭服务器         [3]重启服务器     "
+		echo "  [1]启动服务器      [2]关闭服务器       [3]更改配置文件     "
 		echo "                                          	                 "
-		echo "  [4]手动更新服务器     [5]重新安装环境       [6]获取最新脚本   "
+		echo "  [4]重启服务器      [5]重新安装环境     [6]手动更新服务器   "
+		echo "                                          	                 "
+		echo "  [7]获取最新脚本     "
 		echo "                                          	                 "
 		echo "============================================================"
 		echo "                                                                                  "
@@ -305,15 +496,18 @@ main() {
 				close_server
 				;;
 			3)
-				restart_server
+				change_config
 				;;
 			4)
-				update_game
+				restart_server
 				;;
 			5)
 				init
 				;;
 			6)
+				update_game
+				;;
+			7)
 				get_latest_script
 				;;
 			esac)
